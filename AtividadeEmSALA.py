7fc81c6a6f45adad12eb908410a0a170338e0292
githugro <dsohugo@gmail.com>
2023-10-10 20:00:52 -0300

    Update AtividadeEmSALA.py

--- AtividadeEmSALA.py
+++ AtividadeEmSALA.py
@@ -21,25 +21,47 @@ def imprimir_contatos(contatos):
        
         print(f"****************************")
         print(f"")
+   
+def deletar_contatos(contatos, indice):
+    if 0 <= indice <= len(contatos):
+        del contatos[indice]
+        print("Contato apagado")
+    else:
+        print("Contato não encontrado!")
 
+def editar_contatos(contatos, indice):
+    if 0 <= indice <= len(contatos):
+              
+        print(f"****************************")
+        print("")
+        contatos[indice]['Nome'] = input("Digite o novo nome do contato: ")
+        contatos[indice]['Telefone'] = input("Digite o novo telefone do contato: ")
+        contatos[indice]['E-mail'] = input("Digite o novo e-mail do contato: ")
+
+        print("Contato atualizado!")
+        print(f"****************************")
+        print("")
+    else:
+        print("ID de contato não encontrado!")
 contatos = []
 
 while True:
-    opc = int(input(f"Olá, bem vindo a lista de contatos \n Escolha uma opção: \n 1 - Cadastrar contato; \n 2 - Imprimir contatos cadastrados; \n 3 - Editar contatos; \n  4 - Deletar contato; \n 5 - Sair... Bye bye "))
+    opc = int(input(f"Olá, bem vindo a lista de contatos \n Escolha uma opção: \n 1 - Cadastrar contato; \n 2 - Imprimir contatos cadastrados; \n 3 - Editar contatos; \n 4 - Deletar contato; \n 5 - Sair... Bye bye \n"))
 
     if opc == 1:
 
         nome = input("Digite o nome:    ")
-        telefone = float(input("Digite o número de telefone:    "))
-        email = int(input("Digite o e-mail do arrombado:    "))
+        telefone = input("Digite o número de telefone:    ")
+        email = input("Digite o e-mail do arrombado:    ")
         
-
         cadastrar_contatos(contatos, nome, telefone, email)
     elif opc == 2:
         imprimir_contatos(contatos)
     elif opc == 3:
-        editar_contatos(contatos, indice, nome, telefone, email)
+        indice = int(input("Digite o ID do contato: "))-1
+        editar_contatos(contatos, indice)
     elif opc == 4:
-        deletar_contatos()
+        indice = int(input("Digite o ID do contato: "))-1
+        deletar_contatos(contatos, indice)
     else:
         break
